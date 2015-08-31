c92099f943b7730a9f3287c142a24b4f8de169d6
Simon van Dyk <simon@platform45.com>
2015-08-31 23:35:19 +0200

    Initial Commit

---
+++ Humon/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Humon
+//
+//  Created by Simon Van Dyk on 2015/08/31.
+//  Copyright (c) 2015 Simon van Dyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
